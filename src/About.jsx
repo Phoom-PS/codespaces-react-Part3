92ffcc6f7115843e8dec2e08217f86d5c443960e
Phoom-PS <155616303+Phoom-PS@users.noreply.github.com>
2024-10-03 08:57:00 +0000

    Initial Version

---
+++ src/About.jsx
@@ -0,0 +1,8 @@
+
+function About() {
+    return (
+          <p>From About</p>
+    );
+  }
+  
+  export default About;
---
+++ src/Home.jsx
@@ -0,0 +1,8 @@
+
+function Home() {
+  return (
+        <p>From Homepage</p>
+  );
+}
+
+export default Home;
